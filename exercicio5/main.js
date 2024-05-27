26dba794b609528875e5dacb5c6d4535ab83d6f4
RogerBarretoUnipar <164963528+RogerBarretoUnipar@users.noreply.github.com>
2024-05-27 19:18:13 -0300

    exercicio 5
    funcionando

--- exercicio5/main.js
+++ exercicio5/main.js
@@ -7,12 +7,44 @@ let idade3 = document.querySelector("#idade3");
 let btn = document.querySelector("#btn");
 let resultado = document.querySelector("#resultado");
 
+const pessoas = [];
 
-const idades = [idade1.value, idade2.value, idade3.value];
+function calculaIdade() {
+    const pessoa1Nome = pessoa1.value;
+    const pessoa2Nome = pessoa2.value;
+    const pessoa3Nome = pessoa3.value;
+    const anoAtual = new Date().getFullYear(); 
 
-function calculaidade()
-{
-    alert(idades);
+    const pessoa1Idade = anoAtual - parseInt(idade1.value);
+    const pessoa2Idade = anoAtual - parseInt(idade2.value);
+    const pessoa3Idade = anoAtual - parseInt(idade3.value);
+
+    const pessoa1Obj = { nome: pessoa1Nome, idade: pessoa1Idade };
+    const pessoa2Obj = { nome: pessoa2Nome, idade: pessoa2Idade };
+    const pessoa3Obj = { nome: pessoa3Nome, idade: pessoa3Idade };
+
+    pessoas.push(pessoa1Obj, pessoa2Obj, pessoa3Obj);
+
+    pessoas.sort((a, b) => b.idade - a.idade); 
+
+    resultado.innerHTML = "";
+    pessoas.forEach((pessoa, index) => {
+        let posicao = "";
+        switch(index) {
+            case 0:
+                posicao = "Pessoa mais velha";
+                break;
+            case 1:
+                posicao = "Segunda pessoa mais velha";
+                break;
+            case 2:
+                posicao = "Terceira pessoa mais velha";
+                break;
+            default:
+                break;
+        }
+        resultado.innerHTML += `${posicao}: ${pessoa.nome}, idade: ${pessoa.idade} anos<br>`;
+    });
 }
 
-btn.addEventListener("click", calculaidade);
+btn.addEventListener("click", calculaIdade);
